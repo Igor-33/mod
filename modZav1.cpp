2b67e7772c74aa11e7bc428750684f0f8c444d62
Igor-33 <igorbazyk6@gmail.com>
2016-04-08 08:28:41 +0300

    Added files via upload

---
+++ modZav1.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+
+
+int main()
+{
+    int x,y,a=-35,b=40 ;
+    double dovzina;
+    printf("x:");
+    scanf("%d", &x);
+    printf("y:");
+    scanf("%d", &y);
+    dovzina=sqrt((x-a)*(x-a)+(y-b)*(y-b));
+    printf("dovzina=%lf", dovzina);
+    
+    
+    
+    system("PAUSE");
+    return 0;
+}
